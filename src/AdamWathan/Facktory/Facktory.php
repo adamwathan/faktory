b73412a7cf9a0e53de842363aef379f8a4556dad
Adam Wathan <adam.wathan@gmail.com>
2014-04-20 09:14:41 -0400

    Tiny bit of cleanup, kill all the tabs, spaces forever

--- src/AdamWathan/Facktory/Facktory.php
+++ src/AdamWathan/Facktory/Facktory.php
@@ -2,46 +2,46 @@
 
 class Facktory
 {
-	protected static $factories = [];
+    protected static $factories = [];
 
-	public static function add($name, $definitionCallback)
-	{
-		list($name, $model) = static::extractNameAndModel($name);
-		$factory = new Factory($model);
-		$definitionCallback($factory);
-		static::$factories[$name] = $factory;
-	}
+    public static function add($name, $definitionCallback)
+    {
+        list($name, $model) = static::extractNameAndModel($name);
+        $factory = new Factory($model);
+        $definitionCallback($factory);
+        static::$factories[$name] = $factory;
+    }
 
-	protected static function extractNameAndModel($name)
-	{
-		if (! is_array($name)) {
-			return [$name, $name];
-		}
-		return [$name[0], $name[1]];
-	}
+    protected static function extractNameAndModel($name)
+    {
+        if (! is_array($name)) {
+            return [$name, $name];
+        }
+        return [$name[0], $name[1]];
+    }
 
-	public static function build($name, $attributes = [])
-	{
-		return static::getFactory($name)->build($attributes);
-	}
+    public static function build($name, $attributes = [])
+    {
+        return static::getFactory($name)->build($attributes);
+    }
 
-	public static function create($name, $attributes = [])
-	{
-		return static::getFactory($name)->create($attributes);
-	}
+    public static function create($name, $attributes = [])
+    {
+        return static::getFactory($name)->create($attributes);
+    }
 
-	public static function buildList($name, $count, $attributes = [])
-	{
-		return static::getFactory($name)->buildList($count, $attributes);
-	}
+    public static function buildList($name, $count, $attributes = [])
+    {
+        return static::getFactory($name)->buildList($count, $attributes);
+    }
 
-	public static function createList($name, $count, $attributes = [])
-	{
-		return static::getFactory($name)->createList($count, $attributes);
-	}
+    public static function createList($name, $count, $attributes = [])
+    {
+        return static::getFactory($name)->createList($count, $attributes);
+    }
 
-	protected static function getFactory($model)
-	{
-		return static::$factories[$model];
-	}
+    protected static function getFactory($model)
+    {
+        return static::$factories[$model];
+    }
 }
--- src/AdamWathan/Facktory/Factory.php
+++ src/AdamWathan/Facktory/Factory.php
@@ -2,97 +2,101 @@
 
 class Factory
 {
-	protected $model;
-	protected $attributes;
-	protected $sequence = 0;
+    protected $model;
+    protected $attributes;
+    protected $sequence = 0;
 
-	public function __construct($model, $attributes = [])
-	{
-		$this->model = $model;
-		$this->attributes = $attributes;
-	}
+    public function __construct($model, $attributes = [])
+    {
+        $this->model = $model;
+        $this->attributes = $attributes;
+    }
 
-	public function __set($key, $value)
-	{
-		$this->setAttribute($key, $value);
-	}
+    public function __set($key, $value)
+    {
+        $this->setAttribute($key, $value);
+    }
 
-	public function __get($key)
-	{
-		return $this->getAttribute($key);
-	}
+    public function __get($key)
+    {
+        return $this->getAttribute($key);
+    }
 
-	protected function getAttribute($key)
-	{
-		if (is_callable($this->attributes[$key])) {
-			return $this->attributes[$key]($this, $this->sequence);
-		}
-		return $this->attributes[$key];
-	}
+    protected function getAttribute($key)
+    {
+        if (is_callable($this->attributes[$key])) {
+            return $this->attributes[$key]($this, $this->sequence);
+        }
+        return $this->attributes[$key];
+    }
 
-	public function setAttributes($attributes)
-	{
-		$this->attributes = $attributes;
-	}
+    public function setAttributes($attributes)
+    {
+        $this->attributes = $attributes;
+    }
 
-	protected function setAttribute($key, $value)
-	{
-		$this->attributes[$key] = $value;
-	}
+    protected function setAttribute($key, $value)
+    {
+        $this->attributes[$key] = $value;
+    }
 
-	public function build($override_attributes)
-	{
-		$instance = $this->newModel();
-		$attributes = array_merge($this->attributes, $override_attributes);
-		foreach ($attributes as $attribute => $value) {
-			if (is_callable($value)) {
-				$instance->{$attribute} = $value($this, $this->sequence);
-				continue;
-			}
-			$instance->{$attribute} = $value;
-		}
-		$this->sequence++;
-		return $instance;
-	}
+    public function build($override_attributes)
+    {
+        $instance = $this->newModel();
+        $attributes = array_merge($this->attributes, $override_attributes);
+        foreach ($attributes as $attribute => $value) {
+            $instance->{$attribute} = $this->getAttributeValue($value);
+        }
+        $this->sequence++;
+        return $instance;
+    }
 
-	public function buildList($count, $override_attributes)
-	{
-		return array_map(function($i) use ($override_attributes) {
-			return $this->build($override_attributes);
-		}, range(1, $count));
-	}
+    protected function getAttributeValue($value)
+    {
+        if (is_callable($value)) {
+            return $value($this, $this->sequence);
+        }
+        return $value;
+    }
 
-	public function create($override_attributes)
-	{
-		$instance = $this->build($override_attributes);
-		$instance->save();
-		return $instance;
-	}
+    public function buildList($count, $override_attributes)
+    {
+        return array_map(function($i) use ($override_attributes) {
+            return $this->build($override_attributes);
+        }, range(1, $count));
+    }
 
-	public function createList($count, $override_attributes)
-	{
-		return array_map(function($i) use ($override_attributes) {
-			return $this->create($override_attributes);
-		}, range(1, $count));
-	}
+    public function create($override_attributes)
+    {
+        $instance = $this->build($override_attributes);
+        $instance->save();
+        return $instance;
+    }
 
-	public function sequence($attribute, $callback)
-	{
-		$this->setAttribute($attribute, $callback);
-	}
+    public function createList($count, $override_attributes)
+    {
+        return array_map(function($i) use ($override_attributes) {
+            return $this->create($override_attributes);
+        }, range(1, $count));
+    }
 
-	protected function newModel($attributes = [])
-	{
-		$model = $this->model;
-		return new $model($attributes);
-	}
+    public function sequence($attribute, $callback)
+    {
+        $this->setAttribute($attribute, $callback);
+    }
 
-	public function add($name, $definitionCallback)
-	{
-		$callback = function($f) use ($definitionCallback) {
-			$f->setAttributes($this->attributes);
-			$definitionCallback($f);
-		};
-		Facktory::add([$name, $this->model], $callback);
-	}
+    protected function newModel($attributes = [])
+    {
+        $model = $this->model;
+        return new $model($attributes);
+    }
+
+    public function add($name, $definitionCallback)
+    {
+        $callback = function($f) use ($definitionCallback) {
+            $f->setAttributes($this->attributes);
+            $definitionCallback($f);
+        };
+        Facktory::add([$name, $this->model], $callback);
+    }
 }
